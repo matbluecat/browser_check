ad31b90d0144c1f449bf6b22e579070faac0fcdd
matbluecat <dev.matbluecat@gmail.com>
2013-01-31 18:16:04 +0900

    iOSにも対応してみた ブラウザ情報をBrowserInfoを継承したクラスのインスタンスとして返すようにしてみた

--- browser_check.php
+++ browser_check.php
@@ -5,43 +5,91 @@
 
 class BrowserCheck
 {
-	public function check($_server=null){
-		$_server = (is_array($_server)) ? $_server : $_SERVER;
-		$_kind = $this->extractKind($_server);
-		return $_kind;
-	}
+	public $os = '';
+	public $os_version = '';
+	public $kind = '';
 
-	/**
-	 * HTTP_USER_AGENT から 機種情報を抜き出してくる
-	 */
-	public function extractKind($_server){
-		$_ret_val = false;
-		if($_server['HTTP_USER_AGENT']!=''){
-			if(strstr($_server['HTTP_USER_AGENT'], 'Android')){
-				if(strstr($_server['HTTP_USER_AGENT'], 'Opera Mini')){
-					// Opera Mini
-					$_user_agent = $_server['HTTP_X_OPERAMINI_PHONE_UA'];
-				}else if(strstr($_server['HTTP_USER_AGENT'], 'Opera Mobi')){
-					// Opera Mobile
-					$_user_agent = $_server['HTTP_DEVICE_STOCK_UA'];
-				}else if(strstr($_server['HTTP_USER_AGENT'], 'Firefox')){
-					// Firefox
-					return 'Firefox';
-				}else{
-					// Default Browser
-					$_user_agent = $_server['HTTP_USER_AGENT'];
+	private static $_os_class_list = array(
+		'Android'	=> 'BrowserInfoAndroid',
+		'iPhone'	=> 'BrowserInfoIos',
+		'iPad'		=> 'BrowserInfoIos',
+		'iPod'		=> 'BrowserInfoIos',
+	);
+
+	public static function getInfo($server=null){
+		$server = (is_array($server)) ? $server : $_SERVER;
+		if($server['HTTP_USER_AGENT']!=''){
+			$class_name = false;
+			foreach(self::$_os_class_list as $os => $name){
+				if(strstr($server['HTTP_USER_AGENT'], $os)){
+					$class_name = $name;
+					break;
 				}
-				preg_match('/(\(.*?\))/', $_user_agent, $matches);
-				if($matches[1]!=''){
-// TODO ついでにバージョン情報も抜き出したい
-					$_tmp = explode(';', trim($matches[1], '()'));
-					$_ret_val = trim(end($_tmp));
-				}
-				var_dump($_tmp);
+			}
+			if($class_name!==false){
+				var_dump($class_name);
+				return new $class_name($server);
 			}
 		}
+		return false;
+	}
+}
 
-		return $_ret_val;
+class BrowserInfoBase
+{
+	public function __construct($server=null){
+		if(is_array($server)){
+			$this->extractKindVersion($server);
+		}
+	}
+}
+
+class BrowserInfoAndroid extends BrowserInfoBase
+{
+	public $os = 'Android';
+	public function extractKindVersion($server){
+		if(strstr($server['HTTP_USER_AGENT'], 'Opera Mini')){
+			// Opera Mini
+			$_user_agent = $server['HTTP_X_OPERAMINI_PHONE_UA'];
+		}else if(strstr($server['HTTP_USER_AGENT'], 'Opera Mobi')){
+			// Opera Mobile
+			$_user_agent = $server['HTTP_DEVICE_STOCK_UA'];
+		}else if(strstr($server['HTTP_USER_AGENT'], 'Firefox')){
+			// Firefox
+			return 'Firefox';
+		}else{
+			// Default Browser
+			$_user_agent = $server['HTTP_USER_AGENT'];
+		}
+		preg_match('/(\(.*?\))/', $_user_agent, $matches);
+		if($matches[1]!=''){
+			$_tmp = explode(';', trim($matches[1], '()'));
+			// OSのバージョン取得
+			foreach($_tmp as $key => $val){
+				if(strstr($val, 'Android')!==false){
+					$_version_tmp = explode(' ', $val);
+					$this->os_version = trim(end($_version_tmp));
+				}
+			}
+			// 機種名取得
+			$_kind_tmp = trim(end($_tmp));
+			$_kind_tmp_array = explode(' ', $_kind_tmp);
+			$this->kind = trim($_kind_tmp_array[0]);
+			$_ret_val = trim(end($_tmp));
+		}
+		$this->os = 'Android';
+		var_dump($_tmp);
+	}
+}
+
+class BrowserInfoIos extends BrowserInfoBase
+{
+	public $os = 'iOS';
+	public function extractKindVersion($server){
+		preg_match('/\((.*?);/', $server['HTTP_USER_AGENT'], $matches);
+		$this->kind = $matches[1];
+		preg_match('/OS (.*?) like/', $server['HTTP_USER_AGENT'], $matches);
+		$this->os_version = trim(str_replace('_', '.', $matches[1]));
 	}
 }
 
--- test_browser_check.php
+++ test_browser_check.php
@@ -57,10 +57,76 @@ $user_agent_list = array(
 	array(
 		'HTTP_USER_AGENT' => 'Mozilla/5.0 (Linux; U; Android 4.0.3; ja-jp; ISW13F Build/V69R51I) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
 	),
+
+	// iPhone Safari
+	// OS 3.0
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16',
+	),
+	// OS 4.0
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7',
+	),
+	// OS 4.0.2
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0_2 like Mac OS X; ja-jp) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A400 Safari/6531.22.7',
+	),
+	// OS 4.1
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_1 like Mac OS X; ja-jp) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8B117 Safari/6531.22.7',
+	),
+	// OS 4.2.1
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; ja-jp) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148a Safari/6533.18.5',
+	),
+	// OS 5.0.1
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A405 Safari/7534.48.3',
+	),
+	// OS 5.1
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3',
+	),
+	// OS 6.0
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25',
+	),
+	// iPad Safari
+	// OS 3.2
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; ja-jp) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B367 Safari/531.21.10',
+	),
+	// OS 3.2.2
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPad; U; CPU OS 3_2_2 like Mac OS X; ja-jp) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B500 Safari/531.21.10',
+	),
+	// OS 4.2.1
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; ja-jp) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5',
+	),
+	// OS 5.0.1
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPad; CPU OS 5_0_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A405 Safari/7534.48.3',
+	),
+	// OS 5.1
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPad; CPU OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B176 Safari/7534.48.3',
+	),
+	// OS 6.0
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25',
+	),
+	// iPod Safari
+	array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPod; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16',
+	),
 );
 
 $bc = new BrowserCheck();
 
 foreach($user_agent_list as $key => $val){
-	var_dump($bc->check($val));
+	$bi = BrowserCheck::getInfo($val);
+	var_dump(
+	$bi
+	);
 }
